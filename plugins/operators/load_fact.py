60c97a0e3a4559dc8a8414550f71b8acbbf977df
Esraa Ahmed <37048408+essraahmed@users.noreply.github.com>
2022-09-27 18:26:06 +0200

    Add files via upload

---
+++ plugins/operators/load_fact.py
@@ -0,0 +1,57 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+    facts_sql_template = """
+         DROP TABLE IF EXISTS {destination_table};
+         CREATE TABLE {destination_table} AS
+         SELECT
+             {groupby_column},
+             MAX({fact_column}) AS max_{fact_column},
+             MIN({fact_column}) AS min_{fact_column},
+             AVG({fact_column}) AS average_{fact_column}
+        FROM {origin_table}
+        GROUP BY {groupby_column};
+        """
+
+    
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id = "",
+                 table = "",
+                 fact_sql = "",
+                 append_only = "",
+                 groupby_column = "",
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.fact_sql = fact_sql
+        self.append_only = append_only
+        self.groupby_column = groupby_column
+
+    def execute(self, context):
+        #redshift connection
+        redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
+        
+        if not self.append_only:
+            self.log.info(f"Delete {self.table} fact table")
+            redshift.run(f"DELETE FROM {self.table}") 
+            
+        self.log.info(f"Insert data into {self.table} fact table")
+
+        if self.append_only:
+            fact_sql = (f"INSERT INTO {self.table} {self.fact_sql}")
+            redshift.run(fact_sql)
+
+#       facts_sql = FactsCalculatorOperator.facts_sql_template.format(
+ #           origin_table = self.origin_table,
+  #          destination_table = self.destination,
+   #         fact_column = self.fact_column,
+    #        groupby_column = self.groupby_column)
+        
+     #   redshift.run(facts_sql)
---
+++ plugins/operators/stage_redshift.py
@@ -0,0 +1,64 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow import DAG
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+       
+    #create a task    
+    template_fields = ("s3_key",)
+    copy_sql = """
+            COPY {}
+            FROM '{}'
+            ACCESS_KEY_ID '{}'
+            SECRET_ACCESS_KEY '{}'
+            REGION '{}'
+            JSON '{}' 
+        """
+    
+    
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id ="",
+                 aws_credentials_id = "",
+                 table = "",
+                 s3_bucket = "",
+                 s3_key = "",
+                 region = "",
+                 json = "auto",
+                 *args, **kwargs):
+        
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.region = region    
+        self.json = json
+        
+        
+    def execute(self, context):
+        AWS_hook = AwsHook(self.aws_credentials_id)
+        credentials = AWS_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
+        self.log.info(f"Clearing data from {self.table} Redshift table")
+        redshift.run(f"Delete FROM {self.table} ")
+        
+        self.log.info("Copying Data From S3 To Redshift")
+        
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        self.log.info(s3_path)
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+                self.table, 
+                s3_path, 
+                credentials.access_key,
+                credentials.secret_key, 
+                self.region,
+                self.json
+            )
+        
+        self.log.info(formatted_sql)
+        redshift.run(formatted_sql)
